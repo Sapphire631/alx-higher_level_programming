f46fec852750f8e7d8f232aad378f5df1f349367
root <blessing.nwafor631@gmail.comgit config --global user.name Sapphire631>
2022-11-29 23:08:40 +0000

    rectified mistakes

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
  * insert_node - Inserts a number into a sorted singly-linked list.
@@ -8,25 +8,25 @@ nclude "lists.h"
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+  listint_t *node = *head, *new;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+  new = malloc(sizeof(listint_t));
+  if (new == NULL)
+    return (NULL);
+  new->n = number;
 
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
+  if (node == NULL || node->n >= number)
+    {
+      new->next = node;
+      *head = new;
+      return (new);
+    }
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+  while (node && node->next && node->next->n < number)
+    node = node->next;
 
-	new->next = node->next;
-	node->next = new;
+  new->next = node->next;
+  node->next = new;
 
-0	return (new);
+  return (new);
 }
